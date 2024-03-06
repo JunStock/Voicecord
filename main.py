c2efc7bf654cfdd34de6f51cffff16967d779ac9
FDP user <146457695+JunStock@users.noreply.github.com>
2024-03-06 11:11:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1194106864582004849
+CHANNEL_ID = 1214785540482928651
 SELF_MUTE = True
 SELF_DEAF = False
 
